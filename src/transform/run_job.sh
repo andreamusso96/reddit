8bccf1ab1e45090f9cb698ea27e5fb751c14a10a
andreamusso96 <andreamusso96@gmail.com>
2025-10-24 19:19:40 +0200

    Updated script location

--- src/transform/run_job.sh
+++ src/transform/run_job.sh
@@ -7,5 +7,4 @@
 module load stack/2024-06 python/3.11.6
 source /cluster/home/anmusso/quantization/myenv/bin/activate
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-python $SCRIPT_DIR/job.py "$@"
+python /cluster/home/anmusso/reddit/src/transform/job.py "$@"
