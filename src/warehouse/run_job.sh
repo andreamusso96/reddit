4f0e16bf733ed6cfe583822d7f486888a47cefdd
andreamusso96 <andreamusso96@gmail.com>
2025-11-02 16:43:13 +0100

    Moved job to 16 cores

--- src/warehouse/run_job.sh
+++ src/warehouse/run_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=dbt_spark_local
 #SBATCH --time=03:00:00
-#SBATCH --cpus-per-task=8          
+#SBATCH --cpus-per-task=16          
 #SBATCH --mem-per-cpu=4GB         
 #SBATCH --output=slurm-%j.out
 
