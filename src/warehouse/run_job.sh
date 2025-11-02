c0023cdd4b05507ebe94e2152b1c51fd0f90d54b
andreamusso96 <andreamusso96@gmail.com>
2025-11-02 16:41:06 +0100

    Extended job to comment and submissions

--- src/warehouse/run_job.sh
+++ src/warehouse/run_job.sh
@@ -18,6 +18,6 @@ echo "RAW_COMMENTS_PARQUET_DIR: $RAW_COMMENTS_PARQUET_DIR"
 
 dbt debug
 dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
-dbt run --select submissions submissions_with_keywords
+dbt run --select submissions comments submissions_with_keywords comments_and_submissions_with_keywords
 dbt run-operation print_rowcount --args '{"model":"submissions_with_keywords"}'
 dbt run-operation print_start_end_dates --args '{"model":"submissions_with_keywords"}'
